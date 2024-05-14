fbe882c2d3f787a7d81f3c7e05d0957ec5e756f6
Thomas Viejo <tviejo@bess-f4r4s6.clusters.42paris.fr>
2024-05-14 10:46:00 +0200

    ex25

--- ex25/ft_foreach.c
+++ ex25/ft_foreach.c
@@ -6,18 +6,32 @@
 /*   By: tviejo <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/13 18:11:30 by tviejo            #+#    #+#             */
-/*   Updated: 2024/05/13 23:03:36 by tviejo           ###   ########.fr       */
+/*   Updated: 2024/05/14 10:45:38 by tviejo           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
+/*
+#include <unistd.h>
 
+void	ft_putchar(int	c)
+{
+	write(1, &c, 1);
+}
+*/
 void	ft_foreach(int *tab, int length, void (*f)(int))
 {
 	int	i;
 
 	i = 0;
-	while (i < lenght)
+	while (i < length)
 	{
 		(*f)(tab[i]);
 		i++;
 	}
 }
+/*
+int	main(void)
+{
+	int	tab[5] = {97, 98, 99, 100, 101};
+	ft_foreach(tab, 5, &ft_putchar);
+}
+*/
