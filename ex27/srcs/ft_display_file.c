8ab180bfff933d2923555f8e52324812379b576c
Thomas Viejo <tviejo@paul-f4Br6s3.clusters.42paris.fr>
2024-05-14 19:33:00 +0200

    new makefile

--- ex27/srcs/ft_display_file.c
+++ ex27/srcs/ft_display_file.c
@@ -6,7 +6,7 @@
 /*   By: tviejo <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/14 11:36:20 by tviejo            #+#    #+#             */
-/*   Updated: 2024/05/14 15:38:16 by tviejo           ###   ########.fr       */
+/*   Updated: 2024/05/14 19:22:38 by tviejo           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,7 +19,7 @@ void	ft_putchar(char c)
 	write(1, &c, 1);
 }
 
-void	ft_putstr(char	*str)
+void	ft_putstr(char *str)
 {
 	int	i;
 
@@ -37,19 +37,19 @@ int	main(int argc, char **argv)
 	(void)argc;
 	if (argc < 2)
 	{
-		write(1, "File name missing.", 18);
-		return (0);
+		write(0, "File name missing.", 18);
+		return (-1);
 	}
 	else if (argc > 2)
 	{
-		write(1, "Too many arguments.", 19);
-		return (0);
+		write(0, "Too many arguments.", 19);
+		return (-2);
 	}
 	fd = open(argv[1], O_RDONLY);
 	if (fd < 0)
 	{
-		write(1, "Cannot read file.", 17);
-		return (0);
+		write(0, "Cannot read file.", 17);
+		return (-3);
 	}
 	byte_read = read(fd, &buffer, 10000);
 	ft_putstr(buffer);
